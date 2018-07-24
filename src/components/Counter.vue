dd275eb35eded4becf9cb02f30edceb174d9f12f
Mélissa Loeser <melissa.loeser@gmail.com>
2018-07-24 12:23:43 +0200

    modif counter store

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <span class="count">{{ count }}</span>
-    <Pictures alt="tree-mojo" src="tree-mojo" width="150" @click="increment">Increment</Pictures>
+    <span class="count">{{ totalCookies }}{{ count }}</span>
+    <img alt="tree-mojo" src="../assets/tree-mojo.png" width="550" @click="increment">
   </div>
 </template>
 
 <script>
-  import Pictures from "@/components/Pictures.vue";
   export default {
-    components: {
-      Pictures
-    },
     name: "counter",
     data: function() {
       console.log(this);
       return {
         count: 0
       };
+    },
+    computed: {
+      totalCookies: function () {
+        return this.$store.state.cookies
+      },
     },
     methods: {
       increment: function() {
--- src/store.js
+++ src/store.js
@@ -4,7 +4,16 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    cookies: 0
+  },
+  mutations: {
+    popCookie (state) {
+      state.cookies++
+
+    }
+  },
+  actions: {
+    click
+  }
 });
