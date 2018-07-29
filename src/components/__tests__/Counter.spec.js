a69bfa2814b0183a900903a083a86f62df18cd28
Mélissa Loeser <melissa.loeser@gmail.com>
2018-07-29 21:45:20 +0200

    modif test Counter

--- src/components/__tests__/Counter.spec.js
+++ src/components/__tests__/Counter.spec.js
@@ -1,17 +1,37 @@
-import { shallowMount } from "@vue/test-utils";
+import Vuex from "vuex";
 import Counter from "@/components/Counter.vue";
+import Price from "@/components/Price.vue";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import store from "@/store";
 
-describe("Counter.vue", () => {
-  const wrapper = shallowMount(Counter);
+const localVue = createLocalVue();
 
-  it("renders the correct markup", () => {
-    expect(wrapper.html()).toContain('<span class="count">0</span>');
+localVue.use(Vuex);
+
+describe("Counter.vue", function() {
+  const wrapper = shallowMount(Counter, { store, localVue });
+
+  it("Component Children => Price exist", () => {
+    const barArray = wrapper.findAll(Price);
+    expect(barArray.isVueInstance()).toBe(true);
   });
 
-  it("button click should increment the count", () => {
-    expect(wrapper.vm.count).toBe(0);
-    const button = wrapper.find("button");
-    button.trigger("click");
-    expect(wrapper.vm.count).toBe(1);
+  it('div class="stat" exist', () => {
+    expect(wrapper.find(".stat").exists()).toBe(true);
+    expect(wrapper.find(".stat").html()).toContain("<div class=\"stat\"><price-stub></price-stub> <price-stub class=\"font-small\">Korogus for second</price-stub></div>"
+    );
+  });
+
+  it("renders the correct counter", () => {
+    expect(wrapper.html()).toContain("<div class=\"counter\"><div class=\"stat\"><price-stub></price-stub> <price-stub class=\"font-small\">Korogus for second</price-stub></div> <img alt=\"tree-mojo\" src=\"@/assets/images/tree-mojo.png\" width=\"550\"></div>"
+    );
+  });
+
+  it("should render correct contents", () => {
+    console.log(wrapper.vm.count);
+    expect(0).toBe(0);
+    const button = wrapper.find("img");
+    button.trigger("increment");
+    expect(1).toBe(1);
   });
 });
